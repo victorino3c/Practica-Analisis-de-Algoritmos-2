035dff4de5caf0c3f91dade90f5f83c13f8b54e4
victorino3c <99192011+victorino3c@users.noreply.github.com>
2022-10-21 18:01:35 +0200

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -76,3 +76,80 @@ int min(int* array, int ip, int iu, int *flag)
   return minimo;
 
 }
+
+int mergesort(int* tabla, int ip, int iu)
+{
+  int m;
+
+  if (ip > iu)
+  {
+    return ERR;
+  } 
+  if (ip == iu)
+  {
+    return ob; /*Devolver variable de ob*/
+  } else 
+  {
+    m = (ip+iu)/2;
+    mergesort(tabla, ip, m);
+    mergesort(tabla, m+1, iu);
+
+    return merge(tabla, ip, iu, m);
+  }
+}
+
+int merge(int* tabla, int ip, int iu, int imedio)
+{
+  int *t_aux = NULL;
+  int i = ip, j = imedio+1, k = ip;
+
+  printf("%d-%d\n\n", iu, ip);
+  if (!(t_aux = (int*)malloc(((iu-ip)+1)*sizeof(int))))
+  {
+    return ERR;
+  }
+  printf("2\n\n");
+
+  while (i <= imedio && j <= iu)
+  {
+    ob++;
+    if (tabla[i] < tabla[j])
+    {
+      t_aux[k] = tabla[i];
+      i++;
+    } else 
+    {
+      t_aux[k] = tabla[j];
+      j++;
+    }
+
+    k++;
+  }
+
+  if (i > imedio)
+  {
+    while (j <= iu)
+    {
+      t_aux[k] = tabla[j];
+      j++;
+      k++;
+    }
+  } else if (j > iu)
+  {
+    while (i <= imedio)
+    {
+      t_aux[k] = tabla[i];
+      i++;
+      k++;
+    }
+  }
+
+  for (i = ip; i <= iu; i++)
+  {
+    tabla[i] = t_aux[i];
+  }
+
+  free(t_aux);
+  return ob;
+}
+
